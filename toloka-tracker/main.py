acc6f44889a3b8c566168f7080451c5955c1150e
Andrei Kirilenko <hotckiss@yandex-team.ru>
2021-12-25 15:21:47 +0300

    project base

---
+++ toloka-tracker/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+from fastapi.responses import HTMLResponse
+from dependencies import Base, engine, Container
+
+Base.metadata.create_all(bind=engine)
+
+container = Container()
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return Jinja2Templates(directory="xxx").TemplateResponse("root.html", {"request": request})
